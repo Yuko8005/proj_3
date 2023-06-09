782927708a948b16854cb0dfb2d22166839b9196
Yuko8005 <yukoko6789@gmail.com>
2023-06-09 20:33:56 +0300

    1 commit

---
+++ p_3/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.Win32;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var subKeyNames = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+
+        Console.WriteLine(subKeyNames);
+        string[] valueNames = subKeyNames.GetValueNames();
+        foreach (var item in valueNames)
+        {
+            Console.WriteLine(item);
+            string value = (string)Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run", item, null);
+            Console.WriteLine(value);
+            Console.WriteLine((string)Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run", item, null));
+        }
+    }
+}
